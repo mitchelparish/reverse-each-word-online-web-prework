6aa02d38bc442f84bf7647e22d249533fe38f3d8
Mitchel Parish <mitchelparish@gmail.com>
2018-12-31 20:11:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,16 @@
 def reverse_each_word(sentence)
-  sentence.split(" ")
-    sentence.collect do |word|
+  sentence.collect do |word|
     word.reverse
+  
   end
 end
 
 
-
-
-
+def reverse_each_word(sentence)
+  words = sentence.split(" ")
+    reverse_sentence = []
+  words.each do |word|
+    reverse_sentence << word.reverse
+  end
+  reverse_sentence.join(" ")
+end
