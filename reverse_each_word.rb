bc150a890e84a44a59c7b5f63ed04309ca8b6d08
Mitchel Parish <mitchelparish@gmail.com>
2018-12-31 20:01:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  words = sentence.split
-  reverse_sentence = []
+  words = sentence.split(" ")
+    reverse_sentence = []
   words.each do |word|
     reverse_sentence << word.reverse
   end
-  reverse_sentence.join
+  reverse_sentence.join(" ")
 end
